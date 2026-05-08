040f2f443ed4c53c8f44ae5b3e57d0709cdd7279
Eduardo-fpf <eduardocfrota901@gmail.com>
2026-05-08 11:31:23 -0400

    feat: Foi implementado o arquivo de post no facebook com a biblioteca pyautogui.

--- main.py
+++ main.py
@@ -1,3 +1,43 @@
+import time
+import pyautogui
+import csv
+
+
 class AutomacoesTurma01:
     def __init__(self):
-        pass
+        pass 
+
+    def postador_de_posts(self):
+        pyautogui.click(1016,1060)
+        time.sleep(5)
+        pyautogui.click(430,918)
+        time.sleep(5)
+        url = "https://www.facebook.com/"
+        pyautogui.write(url)
+        pyautogui.press("ENTER")
+        time.sleep(5)
+        email = ""
+        senha = ""
+        pyautogui.write(email)
+        pyautogui.press("TAB")
+        pyautogui.write(senha)
+        pyautogui.press("TAB")
+        pyautogui.press("TAB")
+        pyautogui.press("ENTER")
+        time.sleep(40)
+        pyautogui.press("ESC")
+        time.sleep(3)
+        pyautogui.click(453,551)
+        time.sleep(3)
+        with open("textofacebook.txt", "r",encoding="utf-8") as file:
+            conteudo = file.read()
+        pyautogui.write(conteudo)
+        time.sleep(4)
+        pyautogui.click(524,794)
+        print(pyautogui.position())
+
+        
+
+
+run = AutomacoesTurma01()
+run.postador_de_posts()
